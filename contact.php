59089d36a07f8a024731656460ff1f44385a7723
Gooti Gangadhara <68936937+Gangadhar-gdvs@users.noreply.github.com>
2023-05-04 11:37:59 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>JNTU | Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+		<style type="text/css">
+			.top{
+	width:100%;
+	background-color:#2FA5EB !important;
+	border-bottom:0px solid #f0f0f0;
+}
+.top div{
+	width:1200px;
+	color:#fff;
+	background-color:#2FA5EB !important;
+	font-family:calibri;
+	padding-top:5px ;
+	padding-left:100px;
+	padding-bottom:5px ;
+	padding-right:3px ;
+	text-align:right;
+}
+		 	.nav_down{
+	width:100%;
+	background-color:#2FA5EB !important;
+	border-bottom:2px solid #f0f0f0;
+}
+.nav_down div{
+	width:1329px;
+	color:#fff;
+	background-color:#000;
+	font-family:calibri;
+	padding:10px;
+	text-align:left;
+}
+
+		</style>
+	</head>
+	<body style="background-color:whitesmoke;">
+		<div class="top">
+		<div>
+		 Contact IN +91 6300355608 | jntu@hospital.com 
+		</div>
+	</div>
+		
+			<div class="header" style="background: hotpink;padding-bottom: 10px;">
+				<div class="wrap">
+				
+				<div class="logo">
+					 <img src="images/hms1.jpeg"style="border-radius:50px;" ></img>
+					<a href="index.html" style="font-family:Georgia;font-size: 40px;">JNTU Hospital </a>
+				</div>
+			
+				<div class="top-nav" style="padding-top: 10px;">
+					<ul>
+						<li><a href="index.php"style="border-bottom-left-radius: 5px;border-top-left-radius: 5px;">Home</a></li>
+
+						<li class="active"><a href="contact.php"style="border-bottom-right-radius: 5px;border-top-right-radius: 5px;">contact</a></li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+				
+			</div>
+		
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">
+				<div class="col span_1_of_3">
+
+      			<div class="company_address">
+				     	<h2>Hospital Address  :</h2>
+						    	<p>500 Muddanur Road,</p>
+						   		<p>Pulivendula, Kadapa District,</p>
+						   		<p>India</p>
+				   		<p>Phone:(00) 222 666 444</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span>jntuhospital@gmail.com</span></p>
+
+				   </div>
+				</div>
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit" ></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<div class="nav_down">
+		<div>
+		 &copy; JNTU Hospital, site designed & developed by JNTU  CSE  Students
+		</div>
+	</div>
+	</body>
+</html>
